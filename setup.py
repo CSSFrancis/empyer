1f080ba02590de26f5f6138ebb4fd992c059ad99
shaw <csfrancis@wisc.edu>
2019-03-20 09:18:21 -0500

    adding set up tools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='empyer',
+      version='0.1',
+      discription='Electron Microscopy tools for analyzing 4 and 5 dimensional STEM datasets',
+      url='https://github.com/CSSFrancis/empyer',
+      author='CSSFrancis',
+      author_email='csfrancis@wisc.edu',
+      liscense='UWMadison',
+      packages=['empyer'],
+      zip_safe=False)
