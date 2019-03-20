3e0c8ac7640b7646a182d4410780ef3f5fa74639
shaw <csfrancis@wisc.edu>
2019-03-20 10:00:27 -0500

    adding set up tools

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='empyer',
       version='0.1',
-      discription='Electron Microscopy tools for analyzing 4 and 5 dimensional STEM datasets',
+      description='Electron Microscopy tools for analyzing 4 and 5 dimensional STEM datasets',
+      long_description=readme(),
+      keywords='STEM Electron Microscopy Glass',
       url='https://github.com/CSSFrancis/empyer',
       author='CSSFrancis',
       author_email='csfrancis@wisc.edu',
       liscense='UWMadison',
       packages=['empyer'],
       install_requires=['hyperspy','numpy', 'matplotlib', 'h5py'],
+      include_package_data=True,
       zip_safe=False)
