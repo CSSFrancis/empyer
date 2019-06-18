FEM fixed and allows for thickness filtering to be applied. Everything should now be operational, including masking capabilities.

empyer/signals/polar_signal.py
@@ -3,6 +3,7 @@ from empyer.signals.emsignal import EMSignal
 from empyer.signals.correlation_signal import CorrelationSignal
 from empyer.misc.angular_correlation import angular_correlation
 from empyer.misc.image import square
+from hyperspy.utils import stack
 
 
 class PolarSignal(EMSignal):
@@ -100,28 +101,35 @@ class PolarSignal(EMSignal):
                 int_vs_k = (var - center) / center
                 print(int_vs_k.axes_manager)
         else:
-            filter, thickness = self.thickness_filter()
-            with self.unfolded(unfold_navigation=True, unfold_signal=False):
-                if version is 'rings':
-                    int_vs_k = []
-                    for i, th in enumerate(thickness):
-                        var = self.inav[filter == i].nanmean(axis=-1)
-                        var.map(square)
-                        var = var.nanmean()
-                        center = self.nanmean(axis=-1).nanmean()
-                        center.map(square)
-                        int_vs_k = int_vs_k.append((var - center) / center)
-                elif version is 'omega':
-                    int_vs_k = []
-                    for i, th in enumerate(thickness):
-                        index = np.where(filter.flatten(0)==i+1)[0]
-                        print(tuple(index))
-                        var = self.inav[tuple(index)].map(square,
-                                                         show_progressbar=False,
-                                                         inplace=False).nanmean().nanmean(axis=1)
-                        center = self.nanmean(axis=-1)
-                        center.map(square)
-                        center = center.nanmean()
-                        int_vs_k.append((var - center) / center)
+            filt, thickness = self.thickness_filter()
+            if version is 'rings':
+                int_vs_k = []
+                for i, th in enumerate(thickness):
+                    index = np.where(filt.transpose() == i+1)
+                    index = tuple(zip(index[0], index[1]))
+                    var = stack([self.inav[ind] for ind in index])
+                    v = var.nanmean(axis=-1)
+                    v.map(square)
+                    v = v.nanmean()
+                    center = var.nanmean(axis=-1).nanmean()
+                    center.map(square)
+                    int_vs_k.append((v - center) / center)
+            if version is 'omega':
+                int_vs_k = []
+                for i, th in enumerate(thickness):
+                    index = np.where(filt.transpose() == i+1)
+                    index = tuple(zip(index[0], index[1]))
+                    var = stack([self.inav[ind] for ind in index])
+                    v = var.map(square, inplace=False).nanmean().nanmean(axis=-2)
+                    print(v)
+                    center = var.nanmean(axis=-2)
+                    print(center)
+                    center.map(square)
+                    center = center.nanmean()
+                    int_vs_k.append((v - center) / center)
+            int_vs_k = stack(int_vs_k)
+            int_vs_k.axes_manager.navigation_axes[0].offset = thickness[0]
+            int_vs_k.axes_manager.navigation_axes[0].scale = thickness[1] - thickness[0]
+
         return int_vs_k
 

empyer/signals/power_signal.py
@@ -66,9 +66,7 @@ class PowerSignal(EMSignal):
             2 dimensional map of from the power spectrum
         """
         if symmetry is None:
-
             sym_map = self.isig[k_region[0]:k_region[1], :].sum(axis=[-1,-2]).transpose()
         else:
-            print(self.isig[k_region[0]:k_region[1],:].isig[:,[5,6]])
-            sym_map = self.isig[k_region[0]:k_region[1], symmetry].sum(axis=[-1, -2]).transpose()
+            sym_map = self.isig[k_region[0]:k_region[1], symmetry].sum(axis=[-1]).transpose()
         return sym_map

empyer/tests/signal/test_polar_signal.py
@@ -32,5 +32,7 @@ class TestPolarSignal(TestCase):
 
     def test_fem_with_filter(self):
         self.ps.add_hdaaf_intensities(np.random.normal(size=(10, 10)), 1.5, .1)
-        self.ps.fem(version="omega")
+        vari = self.ps.fem(version="omega")
+        self.ps.plot()
+        plt.show()
         self.ps.fem(version="rings")

empyer/tests/signal/test_power_signal.py
@@ -16,17 +16,6 @@ class TestPowerSignal(TestCase):
                          scale=.1,
                          units='nm^-1')
 
-    def test_add_mask(self):
-        self.ps.mask_shape(shape='rectangle', data=[1, 10, 1, 10])
-
-    def test_get_mask(self):
-        self.ps.mask_shape(shape='rectangle', data=[0, 10, 1, 10])
-        self.ps.mask_shape(shape='rectangle', data=[1, 20, -10, -1])
-        mask = self.ps.get_mask()
-        test = np.zeros((720, 180), dtype=bool)
-        test[0:10, 1:10] = 1
-        test[1:20, -10:-1] = 1
-
     def test_i_vs_k(self):
         self.ps.get_i_vs_k()
         self.ps.get_i_vs_k(symmetry=[6])
@@ -36,4 +25,4 @@ class TestPowerSignal(TestCase):
         print(mapped)
         mapped.plot()
         plt.show()
-        self.ps.get_map(symmetry=[10])
+        self.ps.get_map(symmetry=10)