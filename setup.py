a806a5741be16eeadff52228fba720a32ce980d7
shaw <csfrancis@wisc.edu>
2019-07-18 12:33:07 -0500

    Recommit to fix new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='empyer',
-      version='0.200',
+      version='0.201',
       description='Electron Microscopy tools for analyzing 4 and 5 dimensional STEM datasets',
       long_description=readme(),
       keywords='STEM Electron Microscopy Glass',
